11c4beb18badc67544fb762a2b85db740827dc24
AndrWan1 <148342863+AndrWan1@users.noreply.github.com>
2024-03-11 18:21:17 +0200

    3

--- js/task-1.js
+++ js/task-1.js
@@ -1,4 +1,11 @@
 const categories = document.querySelector('#categories')
-console.log(categories)
-const item = document.querySelectorAll('item')
-console.log(item)
+
+let items = document.querySelectorAll('.item')
+
+console.log(`Number of categories: ${items.length}`);
+items.forEach(item => {
+    const categoryName = item.querySelector('h2').textContent;
+    const categoriesElements = item.querySelectorAll('li').length;
+    console.log(`Category: ${categoryName}`)
+    console.log(`Elements: ${categoriesElements}`)
+});
